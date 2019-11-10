b4e06c202cb0e6bb0e8956e5317fe738e8419927
Julien2048 <56035802+Julien2048@users.noreply.github.com>
2019-11-10 11:39:30 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+from 2048_Project.Projet2048 import Carlo
+
+print(Carlo(300))
+
+#################################################
+#Temps d'attente long avant affichage du résultat
+#################################################
